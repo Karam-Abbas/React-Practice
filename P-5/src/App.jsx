de61035fd81b801012f718edf7a7a2e90b2203a9
Karam <70372614+abcd-hud@users.noreply.github.com>
2024-07-26 21:28:34 +0500

    almost done with the coed except one bug of the swap

--- P-5/src/App.jsx
+++ P-5/src/App.jsx
@@ -1,12 +1,70 @@
-import './App.css'
-import {InputBox} from './components/index.js'
+import "./App.css";
+import { useState } from "react";
+import {InputBox}  from "./components/index.js";
+import useCurrencyInfo from "./hooks/useCurrencyInfo.js";
 function App() {
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("pkr");
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swapValues = async () => {
+    setTo(from);
+    setFrom(to);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
   return (
-    <div className='bg-cover w-full h-screen flex items-center justify-center' style={{backgroundImage:`url(https://images.pexels.com/photos/7904953/pexels-photo-7904953.jpeg)`}}>
-      <InputBox/>
+    <div
+      className="bg-cover w-full h-screen flex items-center justify-center"
+      style={{
+        backgroundImage: `url(https://images.pexels.com/photos/7904953/pexels-photo-7904953.jpeg)`,
+      }}
+    >
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox
+                label="from"
+                amount={amount}
+                onAmountChange={(amount) => setAmount(amount)}
+                onCurrencyChange={(currency) => setFrom(currency)}
+                currencyOptions={options}
+                selectedCurrency={from}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5" onClick={swapValues}>Swap</button>
+            </div>
+            <div className="w-full mb-1">
+              <InputBox
+                label="to"
+                amount={convertedAmount}
+                amountDisabled={true}
+                onAmountChange={(amount) => setAmount(amount)}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectedCurrency={to}
+                currencyOptions={options}
+              />
+            </div>
+            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">Convert {from.toUpperCase()} to {to.toUpperCase()}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- P-5/src/components/InputBox.jsx
+++ P-5/src/components/InputBox.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {useId} from "react";
 function InputBox({
   label,
   amount,
@@ -10,11 +10,15 @@ function InputBox({
   amountDisabled = false,
   currencyDisabled = false,
 }) {
+  const id = useId();
   return (
     <div className="bg-white p-3 rounded-lg text-sm flex">
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+        htmlFor={id} 
+        className="text-black/40 mb-2 inline-block">{label}</label>
         <input
+          id={id}
           type="number"
           className="outline-none w-full bg-transparent py-1.5"
           disabled={amountDisabled}
