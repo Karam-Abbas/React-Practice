23ea246a95643667eefb5d2adf52890a5aae9506
Karam <70372614+abcd-hud@users.noreply.github.com>
2024-07-30 12:24:05 +0500

    P-8 done

--- P-8/src/App.jsx
+++ P-8/src/App.jsx
@@ -1,11 +1,37 @@
 import "./App.css";
+import { ThemeContextProvider } from "../contexts/theme";
+import Card from "../components/Card";
+import ThemeBtn from "../components/ThemeBtn";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    document.documentElement.classList.remove("dark", "light");
+    document.documentElement.classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <>
-      <div className="bg-orange-600"> Hello!</div>
-    </>
+    <ThemeContextProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeContextProvider>
   );
 }
 
